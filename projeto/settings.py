61cdaebe9d430365d55ee024946f2d32537e575e
Robertson Ebling dos Santos <robertson@ersistemas.info>
2024-02-27 10:55:58 -0300

    Add domain to ALLOWED_HOSTS

--- projeto/settings.py
+++ projeto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^yh!&qlwvu_23#+xem6d%9454=+*^7&0(rz=nmbis^d=wj@uv+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'chatrealtime.ersistemas.info']
 
 
 # Application definition
